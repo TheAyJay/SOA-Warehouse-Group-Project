5cbe50379315b02b903256a80ec10de79aefa15f
Andrew Jacobson <andrew.jacobson@marquette.edu>
2019-04-23 22:20:05 -0500

    Changed namespace of Product_Prj.Service - it was incorrect and had the same namespace as the WarehouseService

--- Product_Prj.Service/ProductService.cs
+++ Product_Prj.Service/ProductService.cs
@@ -8,7 +8,7 @@ using System.Text;
 using Warehouse_Prj.Logic;
 using Warehouse_Prj.BDO;
 
-namespace Warehouse_Prj.Service
+namespace Product_Prj.Service
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class ProductService : IProduct
--- Product_Prj.Service/Properties/AssemblyInfo.cs
+++ Product_Prj.Service/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Warehouse_Prj.Service")]
+[assembly: AssemblyTitle("Product_Prj.Service")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Warehouse_Prj.Service")]
+[assembly: AssemblyProduct("Product_Prj.Service")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
