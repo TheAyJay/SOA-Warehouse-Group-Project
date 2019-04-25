ed34d6ca6b29e52d567af4b0f19f23bd3f34b074
ytaustin <31577602+ytaustin@users.noreply.github.com>
2019-04-24 20:45:23 -0500

    connect the createupdate with product service

--- CreateUpdate/CreateUpdate.cs
+++ CreateUpdate/CreateUpdate.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.ServiceModel;
-//using CreateUpdate.ProductServiceRef;
+using CreateUpdate.ProductServiceRef;
 // TODO
 // add service reference
 
