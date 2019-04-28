68c760f1e965d58561961faf1c198c224efcc2ef
ytaustin <31577602+ytaustin@users.noreply.github.com>
2019-04-27 20:15:00 -0500

    change all client name to XXXClient. add getproduct by upc function to inbound client

--- WarehouseProject/Inbound.cs
+++ WarehouseProject/Inbound.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.ServiceModel;
+using InboundClient.ProductServiceInbound;
 
 namespace Inbound
 {
@@ -17,6 +19,44 @@ namespace Inbound
             InitializeComponent();
         }
 
+        private void getproduct_Click(object sender, EventArgs e)
+        {
+            var client = new ProductClient();
+            string result = "";
+            try
+            {
+                long productupc = long.Parse(upcbox.Text);
+                Product product = client.GetProductByUPC(productupc);
 
+                var sb = new StringBuilder();
+                //sb.Append("ProductID:" + product.ProductID.ToString() + "\r\n");
+                sb.Append("ProductUPC:" + product.UPC + "\r\n");
+                sb.Append("ProductName:" + product.ProductName + "\r\n");
+                sb.Append("ProductPrice:" + product.UnitPrice.ToString() + "\r\n");
+                //sb.Append("Category:" + product.Category_Category_ID + "\r\n");
+                result = sb.ToString();
+            }
+            catch (TimeoutException ex)
+            {
+                result = "The service operation timed out. " +
+                ex.Message;
+            }
+            catch (FaultException ex)
+            {
+                result = "Unknown Fault: " +
+                ex.ToString();
+            }
+            catch (CommunicationException ex)
+            {
+                result = "There was a communication problem. " +
+                ex.Message + ex.StackTrace;
+            }
+            catch (Exception ex)
+            {
+                result = "Other exception: " +
+                ex.Message + ex.StackTrace;
+            }
+            productbox.Text = result;
+        }
     }
 }
